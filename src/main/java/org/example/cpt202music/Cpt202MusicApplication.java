602ad95578eed62e1b3cd473672f41a4322c6b69
LucioSun <wangzheyusunjun@126.com>
2025-03-22 21:24:21 +0800

    base

---
+++ src/main/java/org/example/cpt202music/Cpt202MusicApplication.java
@@ -0,0 +1,13 @@
+package org.example.cpt202music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpt202MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cpt202MusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cpt202music/Cpt202MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cpt202music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpt202MusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
