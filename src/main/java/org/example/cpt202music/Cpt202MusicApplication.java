7f515864317c5b5236d6d2b41555b8774215696f
aaaaxiaoxu <Liwei.Xu22@student.xjtlu.edu.cn>
2025-03-23 17:08:11 +0800

    feat: 增加了基础代码

--- src/main/java/org/example/cpt202music/Cpt202MusicApplication.java
+++ src/main/java/org/example/cpt202music/Cpt202MusicApplication.java
@@ -1,9 +1,14 @@
 package org.example.cpt202music;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("org.example.cpt202music.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Cpt202MusicApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/cpt202music/controller/MainController.java
@@ -0,0 +1,20 @@
+package org.example.cpt202music.controller;
+
+import org.example.cpt202music.common.BaseResponse;
+import org.example.cpt202music.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
